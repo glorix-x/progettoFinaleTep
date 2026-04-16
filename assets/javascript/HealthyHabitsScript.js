2f70ce3053b4a9ed19d7d14fdb053248f549cc1a
sacchi344 <slorenzo.sacchi@itis.pr.it>
2026-04-16 19:21:26 +0200

    miglioria healthy habits

--- assets/javascript/HealthyHabitsScript.js
+++ assets/javascript/HealthyHabitsScript.js
@@ -22,6 +22,18 @@ const opzioni = [
     ["Meno di 1 ora", "1 - 2 ore", "3 - 4 ore", "Più di 4 ore"],
 ]
 
+const punteggi = [
+    [0, 1, 2, 3, 4],
+    [0, 1, 2, 3],
+    [0, 1, 2, 3],
+    [3, 2, 1, 0],
+    [0, 1, 2, 3],
+    [4, 3, 2, 1, 0],
+    [3, 2, 1, 0],
+    [3, 2, 1, 0],
+    [3, 2, 1, 0],
+]
+
 const domande = [
     "Quante ore dormi in media per notte?",
     "Quante volte fai attività fisica a settimana?",
@@ -34,17 +46,49 @@ const domande = [
     "Quante ore al giorno passi su smartphone/PC fuori dal lavoro/studio?",
 ]
 
+let punteggio_max = 0
+
+for(let op of punteggi) {
+    let max = 0
+    for(let p of op) {
+        if(p > max) {
+            max = p
+        }
+    }
+    punteggio_max += max
+}
+
 let current_question = 0
 const opzioni_per_riga = 3
 let radioBtns = []
 let score = 0
+let previous_score = 0
+
+let inizio = null
+let durata = 500
+
+let next_btn = document.getElementById("next_btn")
+
+function barAnimation(timestamp) {
+    if(inizio == null) {
+        inizio = timestamp
+    }
+    
+    let tempo_passato = timestamp - inizio
+    let progresso = tempo_passato / durata
+
+    document.getElementById("barra_salute").style.width = (previous_score + (score - previous_score) * progresso) / punteggio_max * 100 + "%"
+    document.getElementById("barra_salute").style.background = `rgb(${255 - (previous_score + (score - previous_score) * progresso) / punteggio_max * 255}, ${(previous_score + (score - previous_score) * progresso) / punteggio_max * 255}, 0)`
+
+    if(progresso < 1) {
+        requestAnimationFrame(barAnimation)
+    }
+}
 
 function nextQuestion() {
-    if(current_question >= domande.length) {
-        resocontoFinale()
-        return
-    }
-    if(radioBtns != []) {
+    previous_score = score
+
+    if(current_question != 0) {
         for(let btn of radioBtns) {
             if(btn.checked) {
                 score += parseInt(btn.value)
@@ -52,49 +96,69 @@ function nextQuestion() {
             }
         }
     }
-    radioBtns = []
-    document.getElementById("domanda").innerText = domande[current_question]
-    document.getElementById("emoji").innerText = imgs[current_question]
-    let n = 0
-    let row = document.createElement("div")
-    row.classList.add("choice_row")
-    let choices_box = document.getElementById("choices_box")
 
-    choices_box.innerHTML = ""
+    inizio = null
+    requestAnimationFrame(barAnimation)
+    
+    if(current_question >= domande.length) {
+        resocontoFinale()
+    } else {
+        radioBtns = []
+        document.getElementById("domanda").innerText = domande[current_question]
+        document.getElementById("emoji").innerText = imgs[current_question]
+        let n = 0
+        let row = document.createElement("div")
+        row.classList.add("choice_row")
+        let choices_box = document.getElementById("choices_box")
 
-    for(let str of opzioni[current_question]) {
-        let div = document.createElement("div")
-        div.classList.add("choice")
+        choices_box.innerHTML = ""
 
-        div.appendChild(document.createTextNode(str))
+        for(let str of opzioni[current_question]) {
+            let div = document.createElement("div")
+            div.classList.add("choice")
 
-        let radioButton = document.createElement("input")
-        radioButton.classList.add("radio_choice")
-        radioButton.type = "radio"
-        radioButton.name = "choice"
-        radioButton.value = opzioni[current_question].indexOf(str)
+            div.appendChild(document.createTextNode(str))
 
-        radioBtns.push(radioButton)
-        
-        div.appendChild(radioButton)
-        
-        row.appendChild(div)
-        n++
-        if(n % 3 == 0) {
-            choices_box.appendChild(row)
-            row = document.createElement("div")
-            row.classList.add("choice_row")
+            let radioButton = document.createElement("input")
+            radioButton.classList.add("radio_choice")
+            radioButton.type = "radio"
+            radioButton.name = "choice"
+            let index = opzioni[current_question].indexOf(str)
+            radioButton.value = punteggi[current_question][index]
+
+            radioBtns.push(radioButton)
+            
+            div.appendChild(radioButton)
+            
+            row.appendChild(div)
+            n++
+            if(n % 3 == 0) {
+                choices_box.appendChild(row)
+                row = document.createElement("div")
+                row.classList.add("choice_row")
+            }
         }
+        choices_box.appendChild(row)
+        radioBtns[0].checked = true
+        current_question++
     }
-    choices_box.appendChild(row)
-    radioBtns[0].checked = true
-    current_question++
+}
+
+function reset() {
+    current_question = 0
+    score = 0
+    next_btn.innerText = "Prossimo"
+    next_btn.removeEventListener("click", reset)
+    next_btn.addEventListener("click", nextQuestion)
+    nextQuestion()
 }
 
 function resocontoFinale() {
     document.getElementById("resoconto").innerText = "Il punteggio finale è di " + score
+    next_btn.addEventListener("click", reset)
+    next_btn.innerText = "Ricomincia"
 }
 
-document.getElementById("next_btn").addEventListener("click", nextQuestion)
+next_btn.addEventListener("click", reset)
 
-nextQuestion()
+reset()
